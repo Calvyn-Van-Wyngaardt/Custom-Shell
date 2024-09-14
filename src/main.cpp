76097926ea86accf368504e62bbd94e70902cd45
Sud0man1 <calvyntbx@gmail.com>
2024-09-14 18:26:12 +0200

    Refactored code

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,7 @@
 #include <fcntl.h> 
 
 int main() {
-    int fd;             //Used for opening file
-    int status;         
-
+    int status;
     std::string command = "";
     std::string arg = "";
     
@@ -17,47 +15,16 @@ int main() {
     std::getline(std::cin, arg);
 
     std::cout << "Executing command: " << command << std::endl;
-    pid_t c_id = fork();        //Creating a child process
+    pid_t c_id = fork();
 
     if (c_id == -1) {
         perror("fork");
         exit(EXIT_FAILURE);
     } 
     else if (c_id > 0) {
-        std::cout << "Printed from parent process :) " << getpid() << std::endl;
         while (waitpid(c_id, &status, WNOHANG) == 0) { }
     }
     else {
-        std::cout << "Printed from child process :D " << getpid() << std::endl;
-        fd = open("abc.log", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-
-        if (fd == -1) {
-            perror("Could not open file...");
-            return 1;
-        }
-
-        //stdout
-        if (close(1) == -1) {
-            perror("Error closing stdout");
-            return 1;
-        }
-
-        if (dup2(fd, 1) == -1) {
-            perror("Error copying fd to stdout");
-            return 1;
-        }
-
-        //stderr
-        if (close(2) == -1) {
-            perror("Error closing stderr");
-            return 1;
-        }
-
-        if (dup2(fd, 2) == -1) {
-            perror("Error copying fd to stderr");
-            return 1;
-        }
-        
         //Command building...
         std::string commandStr = "/usr/bin/" + command;
         char* commandChar = &commandStr[0];
@@ -72,8 +39,6 @@ int main() {
 
         //Write operations...
         std::cout << "Command successsfully executed! :D" << std::endl;
-
-        close(fd);
     }
 
     return 0;
